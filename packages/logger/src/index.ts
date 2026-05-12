c440323eb0ed9752bb3b681aa213ea0be684ced0
Alper Çıtak <citak.alper@gmail.com>
2026-05-12 22:39:42 +0200

    create logger package

---
+++ packages/logger/src/index.ts
@@ -0,0 +1,8 @@
+import pino from 'pino';
+
+export const createLogger = (name: string) =>
+  pino({
+    name,
+    level: process.env.LOG_LEVEL ?? 'info',
+    transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined,
+  });
